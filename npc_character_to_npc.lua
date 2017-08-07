66dd5a2ac192bcddd9f4e56fa1e01f7626c55acb
RuffoMaker-PC\RuffoMaker <rubenelinformatico@gmail.com>
2017-08-07 16:02:34 +0200

    Character to NPC

--- npc_character_to_npc.lua
+++ npc_character_to_npc.lua
@@ -13,48 +13,35 @@
 local NpcId = 50000
 local MenuId = 123 -- Unique ID to recognice player gossip menu among others
 local mensaje = "Si quieres aprender a ser GM puedes unirte a nuestra escuela de GM totalmente gratis en http:://www.ruffoschool.com :)"
-local emote = 387
 local milisegundos = 0
 
-local contadorEmote = 0
-local tiempoEmote = 10000
-
-
-local function onCreate(event, creature, diff)
-	if(contadorEmote > tiempoEmote)then
-		contadorEmote = 0
-		creature:Emote(emote)
-	end
-	
-	contadorEmote = contadorEmote + diff
-end
+local personaje = ''
+local subnombre = ''
+local capucha = ''
 
 local function OnGossipHello(event, player, object)
     player:GossipClearMenu() -- required for player gossip
-    player:GossipMenuAddItem(0, "Quiero ser GM!!", 1, 1)
+    player:GossipMenuAddItem(0, "Crear un NPC a partir de un Personaje.", 1, 1)
     player:GossipSendMenu(1, object, MenuId) -- MenuId required for player gossip
 end
 
 local function OnGossipSelect(event, player, creature, sender, intid, code, menuid)
     if (intid == 1) then
-        player:GossipComplete()
-        creature:SendUnitSay(mensaje, 0)
-		--player:SendBroadcastMessage(mensaje)
+        player:GossipMenuAddItem(0, "Escribir el Nombre del Personaje.", 1, 2, true, nil)
+        player:GossipMenuAddItem(0, "Cancelar", 1, 3)
+		player:GossipSendMenu(1, object, MenuId)
+	elseif (intid == 2) then
+		personaje = code
+		creature:sendUnitSay(personaje)
+		player:GossipSendMenu(1, object, MenuId)
+	elseif (intid == 3) then
+		player:GossipComplete()
 	end
-end
-
-local function saludar(event, creature, unit)
-    --milisegundos = milisegundos + diff
-    if(unit ~= nil)then
-        if (creature:IsWithinDistInMap(unit, 15) and unit:IsInAccessiblePlaceFor(creature)) then
-            --creature:CastSpell(unit, 20000)
-            --creature:SendUnitSay(unit:GetUnitType(), 0)
-        end
-    end
-    --if(milisegundos > tiempoSaludar) then
-        --creature:Emote(emoteSaludar)
-        --milisegundos = 0
-    --end
+	
+	
+	--creature:SendUnitSay(mensaje, 0)
+	--player:SendBroadcastMessage(mensaje)
+	--player:GossipComplete()
 end
 
 RegisterCreatureGossipEvent(NpcId, 1, OnGossipHello)
