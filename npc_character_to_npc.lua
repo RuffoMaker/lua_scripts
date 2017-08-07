8ed52b0ad55f0202b9d4c9840df568b59e24394a
RuffoMaker-PC\RuffoMaker <rubenelinformatico@gmail.com>
2017-08-07 15:41:51 +0200

    Character to NPC

---
+++ npc_character_to_npc.lua
@@ -0,0 +1,61 @@
+--[[
+    RuffoSchool <http://www.ruffoschool.com>
+    Eluna Lua Engine <https://github.com/ElunaLuaEngine/Eluna>
+    Eluna Scripts <https://github.com/ElunaLuaEngine/Scripts>
+    Eluna Wiki <http://wiki.emudevs.com/doku.php?id=eluna>
+    -= Script Information =-
+    * Zone: Varias
+    * Script Type: NPC Gossip
+    * Npc: <MJ>Ruffomaker <90000>
+--]]
+
+
+local NpcId = 90001
+local MenuId = 123 -- Unique ID to recognice player gossip menu among others
+local mensaje = "Si quieres aprender a ser GM puedes unirte a nuestra escuela de GM totalmente gratis en http:://www.ruffoschool.com :)"
+local emote = 387
+local milisegundos = 0
+local tiempoEmote = 10000
+
+
+local function onCreate(event, creature)
+	creature:RegisterEvent(emoteMorir, 10000, 0)
+end
+
+local function emoteMorir()
+	creature:Emote(emote)
+end
+
+
+local function OnGossipHello(event, player, object)
+    player:GossipClearMenu() -- required for player gossip
+    player:GossipMenuAddItem(0, "Quiero ser GM!!", 1, 1)
+    player:GossipSendMenu(1, object, MenuId) -- MenuId required for player gossip
+end
+
+local function OnGossipSelect(event, player, creature, sender, intid, code, menuid)
+    if (intid == 1) then
+        player:GossipComplete()
+        creature:SendUnitSay(mensaje, 0)
+		--player:SendBroadcastMessage(mensaje)
+	end
+end
+
+local function saludar(event, creature, unit)
+    --milisegundos = milisegundos + diff
+    if(unit ~= nil)then
+        if (creature:IsWithinDistInMap(unit, 15) and unit:IsInAccessiblePlaceFor(creature)) then
+            --creature:CastSpell(unit, 20000)
+            --creature:SendUnitSay(unit:GetUnitType(), 0)
+        end
+    end
+    --if(milisegundos > tiempoSaludar) then
+        --creature:Emote(emoteSaludar)
+        --milisegundos = 0
+    --end
+end
+
+RegisterCreatureGossipEvent(NpcId, 1, OnGossipHello)
+RegisterCreatureGossipEvent(NpcId, 2, OnGossipSelect)
+
+RegisterCreatureEvent(NpcId, 5, onCreate)
