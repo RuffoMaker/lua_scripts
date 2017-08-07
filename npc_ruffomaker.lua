fafa3364af46717e2b06d81ceb17f1d511b9935b
RuffoMaker <rubenelinformatico@gmail.com>
2017-08-07 13:28:26 +0200

    Ruffomaker npc test

--- npc_ruffomaker.lua
+++ npc_ruffomaker.lua
@@ -35,7 +35,7 @@ end
 local function saludar(event, creature, unit)
     --milisegundos = milisegundos + diff
     if(unit ~= nil)then
-        if (unit:GetUnitType() == "Player" and creature:IsWithinDistInMap(unit, 150) and unit:IsInAccessiblePlaceFor(creature)) then
+        if (creature:IsWithinDistInMap(unit, 150) and unit:IsInAccessiblePlaceFor(creature)) then
             creature:CastSpell(unit, 20000)
             creature:SendUnitSay(mensaje, 0)
         end
