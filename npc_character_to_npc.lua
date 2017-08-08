6874e3ceee7abfbc529ecf45f9bfcf3a0ee389b5
RuffoMaker <rubenelinformatico@gmail.com>
2017-08-08 19:10:35 +0200

    SQL añadido

--- npc_character_to_npc.lua
+++ npc_character_to_npc.lua
@@ -12,7 +12,28 @@
 
 local NpcId = 50000
 local MenuId = 123 -- Unique ID to recognice player gossip menu among others
+local entryMax = 99999
+local entryMin = 90000
 
+-- CONFIGURACION DE LA ROPA
+local casco = 0
+local hombreras = 0
+local camisa = 0
+local pechera = 2586
+local cinturon = 0
+local pantalones 0
+local botas = 11508
+local brazaletes = 0
+local guantes = 0
+local capa = 0
+local tabardo = 0
+local arma1 = 13246
+local arma2 = 0
+local arma3 = 0
+
+-- -------------------------
+
+local prefijoNombre = '<MJ>'
 local personaje = ''
 local subnombre = ''
 local capucha = true
@@ -22,6 +43,7 @@ local emoteFinishCast = 439
 
 local estado = ''
 local entry = '0'
+local outfit = '0'
 local guid = '0'
 local errores = false
 
@@ -39,6 +61,15 @@ local recordatorioDicho = false
 
 local queryBuscarPersonaje = false
 local queryCrearPersonaje = false
+
+local raza = 0
+local clase = 0
+local genero = 0
+local piel = 0
+local cara = 0
+local pelo = 0
+local color = 0
+local barba = 0
 
 
 
@@ -73,12 +104,10 @@ function OnGossipSelect(event, player, object, sender, intid, code, menuid)
 		player:GossipSendMenu(1, object, MenuId)
 	elseif (intid == 5) then
 		capucha = true
-		--crearNPC(player, object)
 		estado = 'buscandoPersonaje'
 		player:GossipComplete()
 	elseif (intid == 6) then
 		capucha = false
-		--crearNPC(player, object)
 		estado = 'buscandoPersonaje'
 		player:GossipComplete()
 	elseif (intid == 3) then
@@ -155,7 +184,7 @@ function creatureAI(event, creature, diff)
 	
 	if(estado == 'recordatorio') then
 		if(recordatorioDicho == false) then
-			creature:SendUnitSay('Recuerda que debes reiniciar el servidor para poder ver los cambios efectuados.', 0)
+			creature:SendUnitSay('Recuerda que debes ejecutar el comando ".reload all starter" para poder spawnearlo.', 0)
 			recordatorioDicho = true
 			tiempo = 0
 		end
@@ -174,6 +203,7 @@ end
 function reset()
 	estado = ''
 	entry = '0'
+	outfit = '0'
 	guid = '0'
 	errores = false
 	tiempo = 0
@@ -186,6 +216,15 @@ function reset()
 	personajeCreadoDicho = false
 	recordatorioDicho = false
 	queryBuscarPersonaje = false
+
+	raza = 0
+	clase = 0
+	genero = 0
+	piel = 0
+	cara = 0
+	pelo = 0
+	color = 0
+	barba = 0
 end
 
 function buscarPersonaje(creature)
@@ -205,31 +244,62 @@ function buscarPersonaje(creature)
 end
 
 function crearPersonaje(creature)
-	queryCrearPersonaje = true
+	local results = CharDBQuery( "SELECT `race`, `class`, `gender`, `skin`, `face`, `hairStyle`, `hairColor`, `facialStyle` FROM `characters` WHERE  UPPER(`name`) =  UPPER('"..personaje.."') LIMIT 1" )
+	if (results) then
+    repeat
+	    raza = results:GetUInt32(0)
+			clase = results:GetUInt32(1)
+			genero = results:GetUInt32(2)
+			piel = results:GetUInt32(3)
+			cara = results:GetUInt32(4)
+			pelo = results:GetUInt32(5)
+			color = results:GetUInt32(6)
+			barba = results:GetUInt32(7)
+  until not results:NextRow()
+
+  if(capucha == false) then
+  	casco = 0
+  end
+
+  local sqlGetNextEntry = "SELECT `entry` + 1 FROM `creature_template` WHERE `entry` BETWEEN '"..entryMin.."' AND '"..entryMax.."' ORDER BY `entry` DESC LIMIT 1;"
+  local resultGetNextEntry = WorldDBQuery(sqlGetNextEntry)
+  if (resultGetNextEntry) then
+  	repeat
+  		entry = resultGetNextEntry:GetUInt32(0)
+  until not resultGetNextEntry:NextRow()
+
+
+  local sqlGetOutfitId = "SELECT MAX(`entry`) + 1 FROM `creature_template_outfits`;"
+  local resultGetOutfitId = WorldDBQuery(sqlGetOutfitId)
+  if (resultGetOutfitId) then
+  	repeat
+  		outfit = resultGetOutfitId:GetUInt32(0)
+  until not resultGetOutfitId:NextRow()
+
+  local sqlCreatureTemplateOutfits = "REPLACE INTO `creature_template_outfits` (`entry`, `race`, `class`, `gender`, `skin`, `face`, `hair`, `haircolor`, `facialhair`, `head`, `shoulders`, `body`, `chest`, `waist`, `legs`, `feet`, `wrists`, `hands`, `back`, `tabard`) VALUES ("..outfit..", "..raza..", "..clase..", "..genero..", "..piel..", "..cara..", "..pelo..", "..color..", "..barba..", "..casco..", "..hombreras..", "..camisa..", "..pechera..", "..cinturon..", "..pantalones..", "..botas..", "..brazaletes..", "..guantes..", "..capa..", "..tabardo..");"
+  local resultCreatureTemplateOutfits = WorldDBQuery(sqlCreatureTemplateOutfits)
+
+  local sqlCreatureTemplate = "REPLACE INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `scale`, `rank`, `dmgschool`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `resistance1`, `resistance2`, `resistance3`, `resistance4`, `resistance5`, `resistance6`, `spell1`, `spell2`, `spell3`, `spell4`, `spell5`, `spell6`, `spell7`, `spell8`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `InhabitType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `DamageModifier`, `ExperienceModifier`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES ("..entry..", 0, 0, 0, 0, 0, -"..outfit..", 0, 0, 0, '"..prefijoNombre..nombre.."', '"..subnombre.."', '', 0, 255, 255, 2, 35, 1, 1, 1.14286, 1, 1, 0, 2000, 2000, 1, 1, 8, 33536, 2048, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, '', 0, 3, 1, 500, 500, 1, 4.6, 1, 0, 0, 1, 0, 0, '', 12340);"
+  local resultCreatureTemplate = WorldDBQuery(sqlCreatureTemplate)
+
+  local sqlCreatureEquipTemplate = "REPLACE INTO `creature_equip_template` (`CreatureID`, `ID`, `ItemID1`, `ItemID2`, `ItemID3`, `VerifiedBuild`) VALUES ("..entry..", 1, "..arma1..", "..arma2..", "..arma3..", 0);"
+	local resultCreatureEquipTemplate = WorldDBQuery(sqlCreatureEquipTemplate)
+
+
+  end
+	
+	if(entry == '0') then
+		lanzarError(creature, 'Oops!! Ha habido un error al intentar crear el NPC...')
+		
+	else
+		queryCrearPersonaje = true
+	end
 end
 
 function lanzarError(creature, mensaje)
 	errores = true
 	creature:SendUnitSay(mensaje, 0)
 	reset()
-end
-
-function crearNPC(player, creature)
-	
-	
-	creature:SendUnitSay('He encontrado el personaje '..personaje..' en la base de datos...', 0)
-	creature:Emote(emoteFinishCast)
-	creature:SendUnitSay('Estoy creando el NPC...', 0)
-	
-	--player:SendBroadcastMessage('El nombre del Personaje es ' .. personaje)
-	--player:SendBroadcastMessage('El subnombre del Personaje es ' .. subnombre)
-	--if(capucha == true) then
-		--player:SendBroadcastMessage('El Personaje lleva la capucha puesta.')
-	--else
-		--player:SendBroadcastMessage('El Personaje no lleva la capucha puesta.')
-	--end
-	
-	WorldDBQuery("SELECT entry, name FROM creature_template LIMIT 10")
 end
 
 RegisterCreatureGossipEvent(NpcId, 1, OnGossipHello)
