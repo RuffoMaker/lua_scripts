ab6461aaaf2f9a9015a1ff31ec959d71bad3ca2c
RuffoMaker <rubenelinformatico@gmail.com>
2017-08-09 16:09:36 +0200

    ScriptHello

--- learn_test/ScriptHello.lua
+++ learn_test/ScriptHello.lua
@@ -1,17 +1,17 @@
 --[[
-	RuffoSchool <http://www.ruffoschool.com>	    
+    RuffoSchool <http://www.ruffoschool.com>        
     Eluna Lua Engine <https://github.com/ElunaLuaEngine/Eluna>
     Eluna Scripts <https://github.com/ElunaLuaEngine/Scripts>
     Eluna Wiki <http://wiki.emudevs.com/doku.php?id=eluna>
-	Date:20170807
-	
-	*== Script Info ==*
-	* Zona: 
-	* Descripcion:
-	* Npc: <MJ> Rasky <90002>
+    Date:20170807
+    
+    *== Script Info ==*
+    * Zona: 
+    * Descripcion:
+    * Npc: <MJ> Rasky <90002>
 ]]--
 
-	--Variables:
+    --Variables:
 local NpcId = 90002
 local MenuId = 123 
 local mensaje = "Aloha !!"
@@ -23,6 +23,9 @@ local function OnGossipHi(event, player, object)
     player:GossipMenuAddItem(0, "Hola amigo!!", 1, 1)
     player:GossipMenuAddItem(0,"No te quiero ni ver!",1,2)
     player:GossipMenuAddItem(0,"A que te meto!!",1,3)
+    player:GossipMenuAddItem(0,"Ir a la casa de subastas de...",1,4)
+    player:GossipMenuAddItem(0,"Pagar un Sicario.",1,5)
+        player:GossipMenuAddItem(0,"Creame un rival digno!.",1,6)
     player:GossipSendMenu(1, object, MenuId)
 end
 
@@ -30,17 +33,25 @@ local function OnGossipSelect(event, player, creature, sender, intid, code, menu
     if (intid == 1) then
         player:GossipComplete()
         creature:SendUnitSay(mensaje, 0)
-		--player:SendBroadcastMessage(mensaje)
-	end
-	if(intid == 2) then
-		 player:GossipComplete()
+        --player:SendBroadcastMessage(mensaje)
+    end
+    if(intid == 2) then
+         player:GossipComplete()
         creature:SendUnitSay(insulto, 0)
     end
     if(intid == 3)then
-    	player:GossipComplete()   	
-    	player = Unit:Attack( who )
-		Unit:Attack( player, false )
+        player:GossipComplete()     
+        --player = Unit:Attack(player )
+        Unit:Attack(player, false )
 
+    end
+    if(intid == 4)then
+         player:GossipClearMenu() 
+         player:GossipMenuAddItem(0, "Ventormenta.!!", 1, 7)
+         player:GossipMenuAddItem(0,"Ogrimar.",1,8)
+         player:GossipMenuAddItem(0,"Darnassus.",1,9)
+         player:GossipMenuAddItem(0,"Forjaz.",1,10)
+         player:GossipMenuAddItem(0,"y mas...",1,11)
     end
 end
 
