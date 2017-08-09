fefcd5cf06248ca03491626ecc26f6c9888ccc8d
rasky0607 <esviasalpa@gmail.com>
2017-08-09 18:54:36 +0200

    reseto de NPC Rasky al morir el player

--- learn_test/ScriptHello.lua
+++ learn_test/ScriptHello.lua
@@ -20,14 +20,13 @@ local insulto = "que te den!"
 
 local function OnGossipHi(event, player, object)
     player:GossipClearMenu() 
-    player:GossipSendMenu(1, object, MenuId)
     player:GossipMenuAddItem(0,"Hola amigo!!", 1, 1)
     player:GossipMenuAddItem(0,"No te quiero ni ver!",1,2)
     player:GossipMenuAddItem(0,"A que te meto!!",1,3)
     player:GossipMenuAddItem(0,"Ir a la casa de subastas de...",1,4)
     player:GossipMenuAddItem(0,"Pagar un Sicario.",1,10)
     player:GossipMenuAddItem(0,"Creame un rival digno!.",1,11)--Hara como el que bebe una pocion y se convertira en algun otro bicho distintas spells
-    
+    player:GossipSendMenu(1, object, MenuId)
 end
 
 local function OnGossipSelect(event, player, creature, sender, intid, code, menuid)
@@ -48,13 +47,12 @@ local function OnGossipSelect(event, player, creature, sender, intid, code, menu
     end
     if(intid == 4)then
          player:GossipClearMenu() 
-         player:GossipSendMenu(2, object, menuid)
          player:GossipMenuAddItem(0,"Ventormenta.!!",2, 5)
          player:GossipMenuAddItem(0,"Ogrimar.",2,6)
          player:GossipMenuAddItem(0,"Darnassus.",2,7)
          player:GossipMenuAddItem(0,"Forjaz.",2,8)
          player:GossipMenuAddItem(0,"y mas...",2,9)
-         
+         player:GossipSendMenu(2, object, menuid)
     end
 end
 
