90dfc145c1492b57499db44de549df9dfe0a4608
rasky0607 <esviasalpa@gmail.com>
2017-08-10 02:19:58 +0200

    Modificacion Npc Rasky

--- learn_test/ScriptHello.lua
+++ learn_test/ScriptHello.lua
@@ -34,8 +34,9 @@ function OnGossipSelect(event, player, creature, sender, intid, code, menuid) --
     end
     if(intid == 2) then
         player:GossipComplete()
-        creature:SendUnitSay("Que te den!", 0)
-        creature:Emote(77)
+        --creature:SendUnitSay("Que te den!", 0)
+        --creature:Emote(14)
+        Unit:SendUnitEmote( "Que te den!", player, 14)
     end
     if(intid == 3)then
         player:GossipComplete()           
