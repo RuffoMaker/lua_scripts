31a3102b49b6dab5ad0ebfc64466af3aea54d29c
RuffoMaker-PC\RuffoMaker <rubenelinformatico@gmail.com>
2017-08-11 20:33:59 +0200

    Test

--- misc/weapon_visuals.lua
+++ misc/weapon_visuals.lua
@@ -7,7 +7,7 @@ This is purely visual fun and the visual will be replaced when the weapon is enc
 This script is 100% automatic. You can only put it to your script folder and it will work.
 ]]
 
-local chance = 0.25
+local chance = 1
 
 -- Do not edit anything below
 
