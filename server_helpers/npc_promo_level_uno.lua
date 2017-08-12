e00e939190a64b2b20b05d43c0483198e66c9a95
RuffoMaker <rubenelinformatico@gmail.com>
2017-08-12 19:50:08 +0200

    npc_promo_level_uno

--- server_helpers/npc_promo_level_uno.lua
+++ server_helpers/npc_promo_level_uno.lua
@@ -4,7 +4,7 @@ local charactersSQL = [[
 		`promocion_id` INT(10) UNSIGNED NOT NULL DEFAULT '0' , 
 		`personaje_id` INT(10) UNSIGNED NOT NULL DEFAULT '0' , 
 		`cuenta_id` INT(10) UNSIGNED NOT NULL DEFAULT '0' , 
-		`ip` INT(20) NOT NULL DEFAULT '0.0.0.0' , 
+		`ip` VARCHAR(20) NOT NULL DEFAULT '0.0.0.0' , 
 		`fecha` INT(10) UNSIGNED NOT NULL DEFAULT '0' , 
 		PRIMARY KEY (`id`)
 	) ENGINE = InnoDB;
