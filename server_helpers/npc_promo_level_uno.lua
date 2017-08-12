5bdcc6f50e95f6c3faf994c032133b2653bac02e
RuffoMaker <rubenelinformatico@gmail.com>
2017-08-12 13:50:13 +0200

    Añadidas las SQL de las promo

---
+++ server_helpers/npc_promo_level_uno.lua
@@ -0,0 +1,51 @@
+local charactersSQL = [[
+	CREATE TABLE IF NOT EXISTS `promociones_entregadas` ( 
+		`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT , 
+		`promocion_id` INT(10) UNSIGNED NOT NULL DEFAULT '0' , 
+		`personaje_id` INT(10) UNSIGNED NOT NULL DEFAULT '0' , 
+		`cuenta_id` INT(10) UNSIGNED NOT NULL DEFAULT '0' , 
+		`ip` INT(20) NOT NULL DEFAULT '0.0.0.0' , 
+		`fecha` INT(10) UNSIGNED NOT NULL DEFAULT '0' , 
+		PRIMARY KEY (`id`)
+	) ENGINE = InnoDB;
+]]
+CharDBQuery(charactersSQL)
+
+local worldSQL = [[
+	CREATE TABLE IF NOT EXISTS `promociones` ( 
+		`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT , 
+		`nombre` VARCHAR(20) NOT NULL DEFAULT '' , 
+		`unica_personaje` TINYINT(1) NOT NULL DEFAULT '0' , 
+		`unica_cuenta` TINYINT(1) NOT NULL DEFAULT '0' , 
+		`unica_ip` TINYINT(1) NOT NULL DEFAULT '0' , 
+		PRIMARY KEY (`id`)
+	) ENGINE = InnoDB;
+]]
+WorldDBQuery(worldSQL)
+
+worldSQL = [[
+	CREATE TABLE IF NOT EXISTS `promocion_requerimientos` ( 
+		`promo_id` INT(20) UNSIGNED NOT NULL , 
+		`nivel` TINYINT(3) NOT NULL DEFAULT '0' , 
+		`nivel_gm` TINYINT(2) NOT NULL DEFAULT '0' , 
+		`oro` INT(20) NOT NULL DEFAULT '0' , 
+		`puntos_honor` INT(20) NOT NULL DEFAULT '0' , 
+		`puntos_arena` INT(20) NOT NULL DEFAULT '0' , 
+		`logro_1` INT(20) NOT NULL DEFAULT '0' , 
+		`logro_2` INT(20) NOT NULL DEFAULT '0' , 
+		`logro_3` INT(20) NOT NULL DEFAULT '0' , 
+		PRIMARY KEY (`promo_id`)
+	) ENGINE = InnoDB;
+]]
+WorldDBQuery(worldSQL)
+
+worldSQL = [[
+	CREATE TABLE IF NOT EXISTS `promocion_recompensas` ( 
+		`id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT , 
+		`promocion_id` INT(10) UNSIGNED NOT NULL , 
+		`tipo_recompensa` VARCHAR(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'item' , 
+		`valor` INT(20) NOT NULL DEFAULT '0' , 
+		PRIMARY KEY (`id`), INDEX (`promocion_id`)
+	) ENGINE = InnoDB;
+]]
+WorldDBQuery(worldSQL)
