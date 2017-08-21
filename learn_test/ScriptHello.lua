eae6ef81b8dd146475ea93933094c9554ae2b999
rasky0607 <esviasalpa@gmail.com>
2017-08-21 14:48:29 +0200

    Modificacion de Npc Rasky

--- learn_test/ScriptHello.lua
+++ learn_test/ScriptHello.lua
@@ -83,7 +83,7 @@ function OnGossipSelect(event, player, creature, sender, intid, code, menuid) --
     ---------------------------- TP a subastas Ventormenta
     if(intid == 7)then
             creature:SendUnitSay("Vete de compras!", 0)
-            player:Teleport(0,-8817.700195, -666.144958, 95.424065, 3.546073)
+            player:Teleport(1,-8817.700195, -666.144958, 95.424065, 3.546073)
             player:GossipComplete()
     end
     -------------------------
