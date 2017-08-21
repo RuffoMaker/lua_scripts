004c7d79d8295ce3937e1cdbd4c6fa571404da17
rasky0607 <esviasalpa@gmail.com>
2017-08-21 18:21:44 +0200

    Modificacion NPC Rasky

--- learn_test/ScriptHello.lua
+++ learn_test/ScriptHello.lua
@@ -138,18 +138,18 @@ function OnGossipSelect(event, player, creature, sender, intid, code, menuid) --
 
       ---------------------------- TP a subastas Entrañas
     if(intid == 12)then
-        quesubastador = math.random(1,2)
+       
         creature:SendUnitSay("Vete de compras!", 0)
 
-        if(quesubastador == 1)then
+       
             player:Teleport(0,1546.590942, 215.702133, -57.34.3666, 2.005278)
         
 
-        elseif(quesubastador == 2)then
+       --[[ elseif(quesubastador == 2)then
             player:Teleport(0,1546.590942, 215.702133, -57.34.3666, 2.005278)
         
 
-       --[[ elseif(quesubastador == 3)then
+        elseif(quesubastador == 3)then
             player:Teleport(0,1546.590942, 215.702133, -57.34.3666, 2.005278)
         
 
