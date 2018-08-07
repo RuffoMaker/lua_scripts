612b87cefa174cd4d9137347496714819e272231
ReturnCero <rlopez@returncero.com>
2018-08-07 03:02:39 +0200

    Lua

--- misc/ventormenta/melris_malagan.lua
+++ misc/ventormenta/melris_malagan.lua
@@ -4,8 +4,16 @@ local function OnDied(event, creature, killer)
   end
 end
 
+local contador = 0
+local contadorMax = 10000
+
 local function OnUpdate(event, creature, diff)
-  creature:SendUnitYell("Haha, I'm out of combat!", 0)
+	contador = contador + diff
+	if(contador > contadorMax) then
+  	creature:SendUnitSay("14 años en el mismo sitio y por fin aprendo a hablar...", 0)
+  	contador = 0
+  end
 end
 
 RegisterCreatureEvent(12480, 4, OnDied)
+RegisterCreatureEvent(12480, 7, OnUpdate)
