ed6ef9da4c74b02077d4966caa6872eae5acb96c
ReturnCero <rlopez@returncero.com>
2018-08-07 02:59:12 +0200

    Lua

--- misc/ventormenta/melris_malagan.lua
+++ misc/ventormenta/melris_malagan.lua
@@ -8,4 +8,5 @@ local function OnUpdate(event, creature, diff)
   creature:SendUnitYell("Haha, I'm out of combat!", 0)
 end
 
-RegisterCreatureEvent(883, 4, OnDied)
+RegisterCreatureEvent(12480, 4, OnDied)
+RegisterCreatureEvent(12480, 7, OnUpdate)
