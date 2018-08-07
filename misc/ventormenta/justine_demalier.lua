64cb0b9a47c772d403dd34fb175761160002bc08
ReturnCero <rlopez@returncero.com>
2018-08-07 16:29:53 +0200

    Reestructuracion

--- misc/ventormenta/justine_demalier.lua
+++ misc/ventormenta/justine_demalier.lua
@@ -1,10 +1,16 @@
 local justineDemalier = {
+	recibe_hola = 101,
+	recibe_saludo = 78,
+	envia_saludo = 66,
 	entry = 12481
 }
 
 function justineDemalier.saludar(event, creature, player, emoteid)
-	creature:SendUnitSay( 'Juan donde esta mi maldicho Chorizo portuano para mi ano!.', 0 )
-	creature:Emote(14)
+	if(emoteid == justineDemalier.recibe_hola) then
+		creature:SendUnitSay("Aloooo",0)
+	elseif(emoteid == justineDemalier.recibe_saludo) then
+		creature:Emote(justineDemalier.envia_saludo)
+	end
 end
 
 RegisterCreatureEvent(justineDemalier.entry, 8, justineDemalier.saludar)
