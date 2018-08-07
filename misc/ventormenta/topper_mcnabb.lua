3a1d9792ac7d6cc3bf4439ce28022364a843407a
ReturnCero <rlopez@returncero.com>
2018-08-08 01:05:15 +0200

    updates

--- misc/ventormenta/topper_mcnabb.lua
+++ misc/ventormenta/topper_mcnabb.lua
@@ -18,7 +18,7 @@ function TopperMcNabb.OnEmote(event, creature, player, emoteid)
 	if(emoteid == TopperMcNabb.emoteRasp) then
 		TopperMcNabb.StartCombat(creature, player)
 	else
-		creature:SendUnitSay(ipairs(TopperMcNabb.frases), 0)
+		creature:SendUnitSay(TopperMcNabb.frases, 0)
 	end
 end
 
