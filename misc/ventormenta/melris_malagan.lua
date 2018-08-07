1b87bf1b9fca008019f8c0f91b849d25675b7395
ReturnCero <rlopez@returncero.com>
2018-08-07 03:18:23 +0200

    Lua

--- misc/ventormenta/melris_malagan.lua
+++ misc/ventormenta/melris_malagan.lua
@@ -10,18 +10,14 @@ local contadorMax = 1000
 local function OnUpdate(event, creature, diff)
 	contador = contador + diff
 	if(contador > contadorMax) then
-		Saludar(creature)
+		friendyUnits = creature:GetFriendlyUnitsInRange(10)
+		for key,value in friendyUnits do
+			if(value:GetObjectType() == "Player") then
+		    creature:SendUnitSay("Hola!!", 0)
+		  end
+		end
   	contador = 0
   end
-end
-
-local function Saludar(creature)
-	friendyUnits = creature:GetFriendlyUnitsInRange(10)
-	for key,value in friendyUnits do
-		if(value:GetObjectType() == "Player") then
-	    creature:SendUnitSay("Hola!!", 0)
-	  end
-	end
 end
 
 RegisterCreatureEvent(12480, 4, OnDied)
