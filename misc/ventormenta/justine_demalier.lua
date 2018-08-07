156f9e7e1c1de0e6cca13e949c3c35883b0dd206
ReturnCero <rlopez@returncero.com>
2018-08-07 16:51:49 +0200

    Reestructuracion

--- misc/ventormenta/justine_demalier.lua
+++ misc/ventormenta/justine_demalier.lua
@@ -10,6 +10,8 @@ function justineDemalier.saludar(event, creature, player, emoteid)
 		creature:SendUnitSay("Aloooo",0)
 	elseif(emoteid == justineDemalier.recibe_saludo) then
 		creature:Emote(justineDemalier.envia_saludo)
+	else
+		creature:SendUnitSay('Ese emote no me gusta.', 0)
 	end
 end
 
