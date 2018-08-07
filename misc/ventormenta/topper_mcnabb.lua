20b778c05b07004f04ff66560ac5266e0d88a131
ReturnCero <rlopez@returncero.com>
2018-08-08 00:12:42 +0200

    updates

--- misc/ventormenta/topper_mcnabb.lua
+++ misc/ventormenta/topper_mcnabb.lua
@@ -6,13 +6,15 @@ local TopperMcNabb = {
 		'Lustro vuestra armadura por una monedita de cobre.',
 		'¿Me podrías dar alguna moneda?',
 		'Todo es culpa del maldito ejército de la Alianza... Tenían que construir sus torres justo detrás de mi granja.',
-		'Todo es culpa de los estúpidos orcos. Tenían que quemar mi granja hasta los cimientos.'
+		'Todo es culpa de los estúpidos orcos. Tenían que quemar mi granja hasta los cimientos.',
+		'¿Una limosna para los pobres?'
 	},
 	tiempoFrase = 180000,
 	emoteRasp = 14
 };
 
 function TopperMcNabb.OnEmote(event, creature, player, emoteid)
+	creature:SendUnitSay('Guapo', 0)
 	if(emoteid == TopperMcNabb.emoteRasp) then -- emote rasp
 		creature:Emote(TopperMcNabb.emoteRasp)
 		creature:SendUnitSay('¿Como? ¡¡TE VAS A ENTERAR!!', 0)
