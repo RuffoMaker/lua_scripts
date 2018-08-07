34cd16fe05bee62d79d9e7b9799007d87644b70d
ReturnCero <rlopez@returncero.com>
2018-08-07 11:43:53 +0200

    Level scaling system

---
+++ misc/level_scaling_system.lua
@@ -0,0 +1,9 @@
+local levelScalingSystem = {};
+
+local function levelScalingSystem.CreatureSetLevel(event, player, enemy) {
+	if(enemy:GetObjectType() == "Creature") then
+		enemy:SetLevel(player:GetLevel())
+	end
+}
+
+RegisterPlayerEvent(33, levelScalingSystem.CreatureSetLevel)
