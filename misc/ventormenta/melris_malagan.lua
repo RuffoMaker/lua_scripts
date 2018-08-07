50d66d6b61df1cfc47d4bf1aafa9b3c4b4a957e6
ReturnCero <rlopez@returncero.com>
2018-08-07 02:57:31 +0200

    Lua

--- misc/ventormenta/melris_malagan.lua
+++ misc/ventormenta/melris_malagan.lua
@@ -1,7 +1,12 @@
 local function OnDied(event, creature, killer)
-    if(killer:GetObjectType() == "Player") then
-        killer:SendBroadcastMessage("You killed " ..creature:GetName().."!")
-    end
+  if(killer:GetObjectType() == "Player") then
+    killer:SendBroadcastMessage("You killed " ..creature:GetName().."!")
+  end
+end
+
+local function OnUpdate(event, creature, diff)
+  creature:SendUnitYell("Haha, I'm out of combat!", 0)
 end
 
 RegisterCreatureEvent(883, 4, OnDied)
+RegisterCreatureEvent(883, 7, OnUpdate)
