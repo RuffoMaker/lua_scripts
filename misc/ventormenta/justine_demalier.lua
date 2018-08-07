fe2a5056b8e896b0fe94f79e0c48dee605249d70
Pablo Lopez <esviasalpa@gmail.com>
2018-08-07 17:02:16 +0200

    p9

--- misc/ventormenta/justine_demalier.lua
+++ misc/ventormenta/justine_demalier.lua
@@ -15,5 +15,11 @@ function justineDemalier.saludar(event, creature, player, emoteid)
 	end
 end
 
-RegisterCreatureEvent(justineDemalier.entry, 8, justineDemalier.saludar)
+--RegisterCreatureEvent(justineDemalier.entry, 8, justineDemalier.saludar)
 
+function justineDemalier.saludar(event, creature, player, emoteid)
+	creature:SendUnitSay( 'Juan donde esta mi maldicho Chorizo portuano para mi ano!.', 0 )
+	creature:Emote(14)
+end
+
+RegisterCreatureEvent(justineDemalier.entry, 8, justineDemalier.saludar)
