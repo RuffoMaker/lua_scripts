8310539acba1a69a84f42be55944d86bcd3e8e65
ReturnCero <rlopez@returncero.com>
2018-08-09 19:40:43 +0200

    Purgatorio

---
+++ zonas/mazmorras/purgatorio/teleport.lua
@@ -0,0 +1,24 @@
+local teleporterPurgatorio = {
+    entry = 90002,
+    frases = {
+        [0] = '¡¡Quiero ir al Purgatorio!!'
+    },
+    MenuId = 123
+};
+
+
+function teleporterPurgatorio.OnGossipHello(event, player, object)
+    player:GossipClearMenu() -- required for player gossip
+    player:GossipMenuAddItem(0, teleporterPurgatorio.frases[0], 1, 1)
+    player:GossipSendMenu(1, object, teleporterPurgatorio.MenuId) -- MenuId required for player gossip
+end
+
+function teleporterPurgatorio.OnGossipSelect(event, player, creature, sender, intid, code, menuid)
+    if (intid == 1) then
+        player:GossipComplete()
+		player:SendBroadcastMessage('funciona')
+	end
+end
+
+RegisterCreatureGossipEvent(NpcId, 1, teleporterPurgatorio.OnGossipHello)
+RegisterCreatureGossipEvent(NpcId, 2, teleporterPurgatorio.OnGossipSelect)
