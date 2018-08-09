4a2f80d858c259b8fd7d76d1c328a2894c5814ce
Pablo Lopez <esviasalpa@gmail.com>
2018-08-09 18:44:32 +0200

    guardia real

--- zonas/bosque_de_elwynn/ventormenta/GuardiaReal.lua
+++ zonas/bosque_de_elwynn/ventormenta/GuardiaReal.lua
@@ -13,11 +13,12 @@ function guardiaReal.OnUpdate(event, creature, diff)
 		for key,value in pairs(friendyUnits) do -- recorro los objetivos amistos
 			if(value:GetObjectType() == "Player") then --compruebo que el objetivo sea un player
 		    	creature:Emote(66) -- hago el emote
-		    	if(math.random(0, 1) == 1) then
+		    	local rnd = math.random(0, 1)
+		    	if(rnd == 1) then
 		    		if(value:GetGender()==0) then
 		    			creature:SendUnitSay("Sr!",0)
 		    		end
-		    		
+
 		    		if(value:GetGender()==1) then
 		    			creature:SendUnitSay("Sra!",0)
 		    		end
@@ -29,4 +30,22 @@ function guardiaReal.OnUpdate(event, creature, diff)
   end
 end
 
+
+ function guardiaReal.OnReceiveEmote(event, creature, player, emoteid)
+	local envia_saludo = 66	
+
+	if(emoteid == recibe_saludo) then
+		
+
+
+
+
+
+		creature:SendUnitSay("¡"..player:GetName().." alistaté en la marina!.", 0) 	
+	end
+	  
+end
+
+
 RegisterCreatureEvent(1756, 7, guardiaReal.OnUpdate)
+-- RegisterCreatureEvent(1756, 8, guardiaReal.OnReceiveEmote)
