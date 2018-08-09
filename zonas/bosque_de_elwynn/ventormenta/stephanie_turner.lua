4c1f041b8f9c27cd38916f646a0f4a75add28ed1
Pablo Lopez <esviasalpa@gmail.com>
2018-08-09 02:27:55 +0200

    p

--- zonas/bosque_de_elwynn/ventormenta/stephanie_turner.lua
+++ zonas/bosque_de_elwynn/ventormenta/stephanie_turner.lua
@@ -41,12 +41,5 @@ function retraso(diff)
 end
 
 
-function setphanie.OnReceiveEmote(event, creature, player, emoteid)
-	 creature:SendUnitSay(emoteid,0)
-	 creature:SendUnitSay(Global:GetCurrTime(),0)
-	 
-end
-
 -- RegisterCreatureEvent(setphanie.entry, 5, setphanie.OnSpawn)
 RegisterCreatureEvent(setphanie.entry, 7, setphanie.OnUpdate)
-RegisterCreatureEvent(setphanie.entry, 8, setphanie.OnReceiveEmote)
