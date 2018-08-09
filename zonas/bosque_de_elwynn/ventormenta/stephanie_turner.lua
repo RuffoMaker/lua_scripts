2cd2a4a2b58fe507c7cd5c889d7fafe9734c9311
ReturnCero <rlopez@returncero.com>
2018-08-09 14:59:28 +0200

    cambios

--- zonas/bosque_de_elwynn/ventormenta/stephanie_turner.lua
+++ zonas/bosque_de_elwynn/ventormenta/stephanie_turner.lua
@@ -20,52 +20,13 @@ function Stephanie.OnUpdate(event, creature, diff)
 	Stephanie.contador = Stephanie.contador + diff
 
 	if(Stephanie.contador > Stephanie.fraseContador) then
-		creature:SendUnitSay('¡Dale a tu cuerpo alegría macarena, que tu cuerpo es pa darle alegría y cosa buena!', 0)
+		creature:SendUnitSay(Stephanie.frases[Stephanie.contadorFrase], 0)
 		Stephanie.contadorFrase = Stephanie.contadorFrase + 1
 
 		Stephanie.contador = 0
 		Stephanie.fraseContador = 4000
 
-		if(Stephanie.contadorFrase == 1) then
-			creature:SendUnitSay('¡Dale a tu cuerpo alegría macarena.¡Eh macarena Ay!',0)
-		elseif(Stephanie.contadorFrase >1)then
-			Stephanie.contadorFrase = 0
-			Stephanie.fraseContador = 10000
-		end
-
-	end		
-
-end
-
-function Stephanie.prueba(event, creature, diff)	
-	Stephanie.contador = Stephanie.contador + diff
-
-	if(Stephanie.contador == 100 or Stephanie.contador<=110 ) then
-		creature:SendUnitSay("hola", 0)
-				
-	end	
-
-	if(Stephanie.contador > 1000 and Stephanie.contador < 1030) then
-			creature:SendUnitSay(Stephanie.contador,0)
-			Stephanie.contador = Stephanie.contador - Stephanie.contador
-	end	
-
-end
-
-
-function Stephanie.prueba2(event, creature, diff)	
-	Stephanie.contador = Stephanie.contador + diff
-
-	if(Stephanie.contador > Stephanie.fraseContador) then
-		creature:SendUnitSay(Stephanie.contador, 0)
-		Stephanie.contadorFrase = Stephanie.contadorFrase + 1
-
-		Stephanie.contador = 0
-		Stephanie.fraseContador = 4000
-
-		if(Stephanie.contadorFrase == 1) then
-			creature:SendUnitSay(Stephanie.contador,0)
-		elseif(Stephanie.contadorFrase >1)then
+		if((Stephanie.contadorFrase > 1)then
 			Stephanie.contadorFrase = 0
 			Stephanie.fraseContador = 10000
 		end
@@ -75,5 +36,5 @@ function Stephanie.prueba2(event, creature, diff)
 end
 
 
--- RegisterCreatureEvent(Stephanie.entry, 5, Stephanie.OnSpawn)
-RegisterCreatureEvent(Stephanie.entry, 7, Stephanie.prueba)
+RegisterCreatureEvent(Stephanie.entry, 5, Stephanie.OnSpawn)
+RegisterCreatureEvent(Stephanie.entry, 7, Stephanie.OnUpdate)
