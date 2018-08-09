503c7efe7bb08bf5cd85a21017ae1d118779bfeb
ReturnCero <rlopez@returncero.com>
2018-08-09 18:16:40 +0200

    Bonus exp

--- misc/leveling/bonus_exp.lua
+++ misc/leveling/bonus_exp.lua
@@ -1,5 +1,6 @@
 local bonusExp = {
-	rate = 10,
+	rateMin = 2,
+	rateMax = 50,
 	chance = 20
 };
 
@@ -7,7 +8,7 @@ function bonusExp.PlayerReceiveExp(event, player, amount, victim)
 	local cantidad = 0
 	if(math.random(0, 100) < bonusExp.chance) then
 		player:SendBroadcastMessage('Has recibido un bonus de experiencia')
-		cantidad = amount * bonusExp.rate
+		cantidad = amount * math.random(bonusExp.rateMin, bonuxExp.rateMax)
 	else
 		cantidad = amount
 	end
