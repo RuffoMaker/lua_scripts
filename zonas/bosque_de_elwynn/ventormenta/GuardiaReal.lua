d811150cae1d2edd65ca0ec58e3632d5c02560db
Pablo Lopez <esviasalpa@gmail.com>
2018-08-09 19:06:47 +0200

    guardia real

--- zonas/bosque_de_elwynn/ventormenta/GuardiaReal.lua
+++ zonas/bosque_de_elwynn/ventormenta/GuardiaReal.lua
@@ -3,7 +3,6 @@ local guardiaReal = {
 	contadorMax = 5000		
 };
 
-
 function guardiaReal.OnUpdate(event, creature, diff)
 	guardiaReal.contador = guardiaReal.contador + diff -- este contado lo uso para el check cada segundo
 	if(guardiaReal.contador > guardiaReal.contadorMax) then -- si ha pasado el segundo
@@ -18,47 +17,4 @@ function guardiaReal.OnUpdate(event, creature, diff)
   end
 end
 
-
- function guardiaReal.OnReceiveEmote(event, creature, player, emoteid)
-	local recibe_saludo = 66	
-
-	if(emoteid == recibe_saludo) then		
-		if(player:GetGender() == 0) then
-		     creature:SendUnitSay(" Sr comandante "..player:GetName().."...", 0)
-		end
-
-		if(player:GetGender() == 1) then
-		    creature:SendUnitSay(" Sra comandante "..player:GetName().."...", 0) 
-		end
-		 	
-	end
-	  
-end
-
-
- function guardiaReal.OnReceiveEmote2(event, creature, player, emoteid)
-	 	
-
-	
-	local recibe_hola = 101
-	local recibe_hola2 = 55
-	local recibe_saludo = 78
-	local envia_saludo = 66	
-
-	if(emoteid == recibe_hola or emoteid == recibe_hola2) then
-		creature:SendUnitSay("Hola cosita linda...",0)
-	elseif(emoteid == recibe_saludo) then
-		creature:Emote(envia_saludo)
-		creature:SendUnitSay("¡"..player:GetName().." alistaté en la marina!.", 0) -- saludo al objetivo
-	else
-		creature:SendUnitSay('Y tu que miras... camina!', 0)
-	end
-		   
-		
-		 	
-	end
-	  
-end
-
 RegisterCreatureEvent(1756, 7, guardiaReal.OnUpdate)
-RegisterCreatureEvent(1756, 8, guardiaReal.OnReceiveEmote2)
