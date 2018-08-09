fd446d69827fb200ee1fcdcf81cc97c4d821c930
Pablo Lopez <esviasalpa@gmail.com>
2018-08-09 14:28:18 +0200

    Stephanie

--- zonas/bosque_de_elwynn/ventormenta/stephanie_turner.lua
+++ zonas/bosque_de_elwynn/ventormenta/stephanie_turner.lua
@@ -41,7 +41,7 @@ function Stephanie.prueba(event, creature, diff)
 	Stephanie.contador = Stephanie.contador + diff
 
 	if(Stephanie.contador == 100 ) then
-		creature:SendUnitSay("¡Dale a tu cuerpo alegría macarena, que tu cuerpo es pa darle alegría y cosa buena", 0)
+		creature:SendUnitSay(Stephanie.contador, 0)
 				
 	end	
 
