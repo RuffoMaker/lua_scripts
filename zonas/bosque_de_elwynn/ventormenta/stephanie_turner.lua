b8bd05c8669434e51e9b80d0c893adb6d7681047
Pablo Lopez <esviasalpa@gmail.com>
2018-08-09 16:14:25 +0200

    Stephanie

--- zonas/bosque_de_elwynn/ventormenta/stephanie_turner.lua
+++ zonas/bosque_de_elwynn/ventormenta/stephanie_turner.lua
@@ -13,9 +13,8 @@ local Stephanie = {
 };
 
 function Stephanie.OnEmote(event, creature, player, emoteid)
-	creature:SendUnitSay(emoteid,0)
 	if(emoteid == Stephanie.emoteDance) then
-		Stephanie.SendUnitSay("¡¡Mirad todos a ".. player:GetName() .. "!! ¡¡Qué estilazo tiene bailando!! ¡¡Vamos allá!!", 0)
+		Stephanie.SendUnitSay('¡¡Mirad todos a ' .. player:GetName() .. '!! ¡¡Qué estilazo tiene bailando!! ¡¡Vamos allá!!', 0)
 		Stephanie.fraseContador = 5000
 	end
 end
