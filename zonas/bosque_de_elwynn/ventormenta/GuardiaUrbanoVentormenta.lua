980e2019a8bc5e29c72f12da3e6682c75af3a543
ReturnCero <rlopez@returncero.com>
2018-08-09 09:42:57 +0200

    Cambios0

--- zonas/bosque_de_elwynn/ventormenta/GuardiaUrbanoVentormenta.lua
+++ zonas/bosque_de_elwynn/ventormenta/GuardiaUrbanoVentormenta.lua
@@ -2,10 +2,14 @@ local guardiaUrbano = {
 	entry = 1976
 }
 
-function guardiaUrbano.OnUpdate(event, creature)			
-		if(creature:isStopped() ==true) then
-			creature:SendUnitSay("Uf..ya queda poco... pronto terminaré el turno de una vez..",0)
-		end				
+function guardiaUrbano.OnSpawn(event, creature)
+	creature:RegisterEvent(guardiaUrbano.Hablar, {0, 10000}, 0)
+end
+
+function guardiaUrbano.Hablar(eventid, delay, repeats, creature)
+	if(creature:IsStopped() == true) then
+		creature:SendUnitSay("Uf..ya queda poco... pronto terminaré el turno de una vez..",0)
+	end
 end
 
 RegisterCreatureEvent(guardiaUrbano.entry, 5, guardiaUrbano.OnUpdate)
