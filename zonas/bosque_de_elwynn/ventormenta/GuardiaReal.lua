2d5cf089d06c254e7037ee4a7e123dad8df3d188
Pablo Lopez <esviasalpa@gmail.com>
2018-08-09 17:59:15 +0200

    guardia real

--- zonas/bosque_de_elwynn/ventormenta/GuardiaReal.lua
+++ zonas/bosque_de_elwynn/ventormenta/GuardiaReal.lua
@@ -1,18 +1,27 @@
-local guardiaReal = {
+local melrisMalagan = {
 	contador = 0,
 	contadorMax = 1000,
 	contadorSaludos = 0,
 	blackList = {}
 };
 
-function guardiaReal.OnUpdate(event, creature, diff)
-	guardiaReal.contador = guardiaReal.contador + diff -- este contado lo uso para el check cada segundo
-	if(guardiaReal.contador > guardiaReal.contadorMax) then -- si ha pasado el segundo
+function melrisMalagan.OnDied(event, creature, killer)
+  if(killer:GetObjectType() == "Player") then
+    creature:SendUnitSay("Hijo de orca...", 0)
+  end
+  creature:RemoveEvents()
+end
+
+
+
+function melrisMalagan.OnUpdate(event, creature, diff)
+	melrisMalagan.contador = melrisMalagan.contador + diff -- este contado lo uso para el check cada segundo
+	if(melrisMalagan.contador > melrisMalagan.contadorMax) then -- si ha pasado el segundo
 		friendyUnits = creature:GetFriendlyUnitsInRange(10) -- obtengo los objetivos amistosos a 10m
 		for key,value in pairs(friendyUnits) do -- recorro los objetivos amistos
 			if(value:GetObjectType() == "Player") then --compruebo que el objetivo sea un player
 				local done = true -- pongo un flag a true
-				for k,v in pairs(guardiaReal.blackList) do -- recorro la blacklist
+				for k,v in pairs(melrisMalagan.blackList) do -- recorro la blacklist
 					v[1] = v[1] - diff -- le resto a esta entrada el tiempo restante para sacarlo (tiempo - diff)
 
 					if(v[0] == value:GetName()) then -- si esta entrada de la blacklist es igual al nombre del objetivo
@@ -27,15 +36,15 @@ function guardiaReal.OnUpdate(event, creature, diff)
 				if(done == true) then -- si el flag esta en true
 		    	creature:SendUnitSay("¡Saludos " .. value:GetName() .. "!", 0) -- saludo al objetivo
 		    	creature:Emote(66) -- hago el emote
-		    	guardiaReal.blackList[guardiaReal.contadorSaludos] = {} -- creo una entrada en la blacklist
-		    	guardiaReal.blackList[guardiaReal.contadorSaludos][0] = value:GetName() -- le pongo el nombre
-		    	guardiaReal.blackList[guardiaReal.contadorSaludos][1] = 100000 -- le pongo el tiempo restante
-		    	guardiaReal.contadorSaludos = guardiaReal.contadorSaludos + 1 -- subo uno al contador
+		    	melrisMalagan.blackList[melrisMalagan.contadorSaludos] = {} -- creo una entrada en la blacklist
+		    	melrisMalagan.blackList[melrisMalagan.contadorSaludos][0] = value:GetName() -- le pongo el nombre
+		    	melrisMalagan.blackList[melrisMalagan.contadorSaludos][1] = 100000 -- le pongo el tiempo restante
+		    	melrisMalagan.contadorSaludos = melrisMalagan.contadorSaludos + 1 -- subo uno al contador
 		    end
 		  end
 		end
-  	guardiaReal.contador = 0 -- pongo el contador de los segundos a 0
+  	melrisMalagan.contador = 0 -- pongo el contador de los segundos a 0
   end
 end
 
-RegisterCreatureEvent(1756, 7, guardiaReal.OnUpdate)
+RegisterCreatureEvent(1756, 7, melrisMalagan.OnUpdate)
