0f8644ed22f9b2e85d7f3022e6c328d8861b4678
ReturnCero <rlopez@returncero.com>
2018-08-09 02:14:46 +0200

    cambios

---
+++ misc/hablar.lua
@@ -0,0 +1,12 @@
+function hablar(frases, entry, tiempo)
+	local tiempoFrase = tiempo
+	local tiempoFraseInit = tiempo
+
+	RegisterCreatureEvent(entry, 7, comienzaHablar)
+end
+
+function comienzaHablar(event, creature, diff)
+	creature:SendUnitSay('hola', 0)
+end
+
+hablar({}, 5406, 1000)
