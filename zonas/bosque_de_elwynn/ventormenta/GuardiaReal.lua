efdf78b3c5ebfe49f2be5924c2631d374eea0495
Pablo Lopez <esviasalpa@gmail.com>
2018-08-09 19:08:35 +0200

    guardia real

--- zonas/bosque_de_elwynn/ventormenta/GuardiaReal.lua
+++ zonas/bosque_de_elwynn/ventormenta/GuardiaReal.lua
@@ -6,7 +6,7 @@ local guardiaReal = {
 function guardiaReal.OnUpdate(event, creature, diff)
 	guardiaReal.contador = guardiaReal.contador + diff -- este contado lo uso para el check cada segundo
 	if(guardiaReal.contador > guardiaReal.contadorMax) then -- si ha pasado el segundo
-		friendyUnits = creature:GetFriendlyUnitsInRange(10) -- obtengo los objetivos amistosos a 10m
+		friendyUnits = creature:GetFriendlyUnitsInRange(8) -- obtengo los objetivos amistosos a 10m
 		for key,value in pairs(friendyUnits) do -- recorro los objetivos amistos
 			if(value:GetObjectType() == "Player") then --compruebo que el objetivo sea un player
 		    	creature:Emote(66) -- hago el emote	    	
