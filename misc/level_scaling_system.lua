df82c7358b0908b567753d74e4bbde35126f13b7
ReturnCero <rlopez@returncero.com>
2018-08-11 00:46:14 +0200

    Cambios

--- misc/level_scaling_system.lua
+++ misc/level_scaling_system.lua
@@ -4,7 +4,7 @@ function levelScalingSystem.CreatureSetLevel(event, player, enemy)
 	--enemy:SendUnitSay('Hola puto', 0)
 	--player:SendUnitSay('Hola puto', 0)
 	--if(enemy:GetObjectType() == "Creature") then
-		enemy:SetLevel(player:GetLevel())
+		--enemy:SetLevel(player:GetLevel())
 	--end
 end
 
