a72699eda6aba747d696eef52bf6523694a05c99
ReturnCero <rlopez@returncero.com>
2018-08-10 18:07:51 +0200

    Cambios

--- misc/saron_story/inicio/first_login.lua
+++ misc/saron_story/inicio/first_login.lua
@@ -12,7 +12,12 @@ function firstLogin.RegistrarInicioDeHistoria(event, player)
 end
 
 function firstLogin.IniciarAtaqueDeGmMalvado(eventid, delay, repeats, player)
-	creature = player:SpawnCreature( firstLogin.creatures.ruffomaker, player:GetX(), player:GetY, player:GetZ(), player:GetO(), 8 ) -- despawn on UnSummon()
+	creature = player:SpawnCreature(firstLogin.creatures.ruffomaker, player:GetX(), player:GetY, player:GetZ(), player:GetO(), 8) -- despawn on UnSummon()
+	player:RegisterEvent(firstLogin.DarElTransformador, 30000)
+end
+
+function firstLogin.DarElTransformador(eventid, delay, repeats, player)
+	player:AddItem(firstLogin.items.transformador, 1)
 end
 
 RegisterPlayerEvent( 30, firstLogin.RegistrarInicioDeHistoria )
