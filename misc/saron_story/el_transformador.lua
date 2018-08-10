23d8fb1bb5ecb68e62e9af02f77489d7a4232bec
ReturnCero <rlopez@returncero.com>
2018-08-11 01:54:12 +0200

    Cambios

--- misc/saron_story/el_transformador.lua
+++ misc/saron_story/el_transformador.lua
@@ -4,7 +4,7 @@ local MenuId = 123
 function initGossip(event, player, item, target)
 	player:GossipClearMenu() -- required for player gossip
   player:GossipMenuAddItem(0, "Quiero ser GM!!", 1, 1)
-  player:GossipSendMenu(1, object, MenuId) -- MenuId required for player gossip
+  player:GossipSendMenu(1, item, MenuId) -- MenuId required for player gossip
 end
 
 RegisterItemEvent(entry, 2, initGossip)
