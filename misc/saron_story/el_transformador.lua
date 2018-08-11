36754884b426dbc6026325809d3a49b414d0a37f
ReturnCero <rlopez@returncero.com>
2018-08-11 02:05:04 +0200

    Cambios

--- misc/saron_story/el_transformador.lua
+++ misc/saron_story/el_transformador.lua
@@ -3,14 +3,18 @@ local MenuId = 123
 
 function initGossip(event, player, item, target)
 	player:GossipClearMenu() -- required for player gossip
-  player:GossipMenuAddItem(0, "Quiero ser GM!!", 1, 1)
+  player:GossipMenuAddItem(0, "Vaca Lechera", 1, 1)
+  player:GossipMenuAddItem(0, "Quiero volver a mi apariencia original.", 1, 2)
   player:GossipSendMenu(1, item, MenuId) -- MenuId required for player gossip
 end
 
 local function OnGossipSelect(event, player, item, sender, intid, code, menuid)
-    if (intid == 1) then
-        player:GossipComplete()
-        player:SetDisplayId(123)
+  if (intid == 1) then
+	  player:GossipComplete()
+	  player:SetDisplayId(1060)
+	else if (intid == 2) then
+		player:GossipComplete()
+		player:DeMorph()
 	end
 end
 
