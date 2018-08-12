764307e3a159f86a2d6723d2297c08752e380fa9
ReturnCero <rlopez@returncero.com>
2018-08-12 19:20:25 +0200

    Cambios

---
+++ zonas/bosque_de_elwynn/villadorada/toddrick.lua
@@ -0,0 +1,20 @@
+local TopperMcNabb = {
+	entry = 3935,
+	frases = {
+		[0] = 'La última vaca que mate tenía una mirada rara... parecía que no quería irse.'
+	},
+	emotes = {
+		picarCarne = 173
+	}
+};
+
+function TopperMcNabb.OnSpawn(event, creature)
+	creature:Emote(TopperMcNabb.emotes.picarCarne)
+	creature:RegisterEvent(TopperMcNabb.Talk, 1000, 0)
+end
+
+function TopperMcNabb.Talk(eventid, delay, repeats, creature)
+	creature:SendUnitSay(TopperMcNabb.frases[0], 0)
+end
+
+RegisterCreatureEvent(TopperMcNabb.entry, 5, TopperMcNabb.OnSpawn)
