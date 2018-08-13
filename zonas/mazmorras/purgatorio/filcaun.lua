461b3f948bf68c950c13fd4f932a58f5baf63472
Pablo Lopez <esviasalpa@gmail.com>
2018-08-13 16:26:49 +0200

    Filcaun

--- zonas/mazmorras/purgatorio/filcaun.lua
+++ zonas/mazmorras/purgatorio/filcaun.lua
@@ -3,6 +3,7 @@ local filcaun = {
 	contadorInicio = 1000,
 	contadorInicioDicho = 0,
 	emoteHablar = 1,
+	idemot = 0,
 	emoteNoHaceNada = 0			
 }
 
@@ -22,10 +23,15 @@ filcaun.sumatorioDetiempo = filcaun.sumatorioDetiempo +diff
 		end
 		filcaun.sumatorioDetiempo = 0
 	else
-	creature:SendUnitSay("En espera...", 0)
+		creature:SendUnitSay("En espera...", 0)
 	end 
 end
 
+ function filcaun.OnReceiveEmote(event, creature, player, emoteid)
+ 		filcaun.idemot = emoteid
+		creature:SendUnitSay(filcaun.idemot,0)
+	  
+end
 
-
+RegisterCreatureEvent(80500, 8, filcaun.OnReceiveEmote)
 RegisterCreatureEvent(80500, 7, filcaun.OnUpdate)
