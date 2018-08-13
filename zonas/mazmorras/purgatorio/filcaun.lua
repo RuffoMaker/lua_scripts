c1249b1cab952c02bc43a292334e917b2fd5a065
Pablo Lopez <esviasalpa@gmail.com>
2018-08-13 16:33:21 +0200

    Filcaun

--- zonas/mazmorras/purgatorio/filcaun.lua
+++ zonas/mazmorras/purgatorio/filcaun.lua
@@ -1,24 +1,15 @@
 local filcaun = {
 	sumatorioDetiempo = 0,
-	contadorInicio = 1000,
-	contadorInicioDicho = 0,
-	emoteHablar = 1,
-	idemot = 0,
-	emoteNoHaceNada = 0			
-};
+	contadorInicio = 1000		
+}
 
 function filcaun.OnUpdate(event, creature, diff)
 
 filcaun.sumatorioDetiempo = filcaun.sumatorioDetiempo +diff
 
 	if(filcaun.contadorInicio < filcaun.sumatorioDetiempo) then
-		if(creature:GetFriendlyUnitsInRange(3) ~= {}) then
-			creature:Emote(filcaun.emoteHablar)
-			creature:SendUnitSay(" ¡Pssss!,¡Pssss!,¡Pssss! ¡por aquí!", 0)
-			creature:SendUnitSay(" No hagaís ruido.Escape por los pelos... ese esbirro de Torcuato... ¡esta incluso más loco que él¡", 0)	
-			creature:SendUnitSay(" Escape graciás aún búfo que conseguir lanzarme que invente yo mismo.., es un poco impreciso aún y los resultados pueden variar..Pero si quereís os lo  lanzare a ustedes también, solo tiene que hablar conmigo todo el que quiera probar..,¡No seais tímidos!.", 0)
-			creature:SendUnitSay(" Como os dije aun esta en fase experimental... pero puede funcionar muy bien...", 0)
-			creature:Emote(filcaun.emoteNoHaceNada)
+		
+			creature:SendUnitSay("Hola...", 0)
 			-- filcaun.contadorInicioDicho = filcaun.contadorInicioDicho + 1
 		end
 		filcaun.sumatorioDetiempo = 0
