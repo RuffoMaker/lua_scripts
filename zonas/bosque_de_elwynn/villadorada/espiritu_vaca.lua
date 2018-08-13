de8655b9803628a91024b570411ba84c4eca5e42
ReturnCero <rlopez@returncero.com>
2018-08-13 10:49:38 +0200

    Cambios

---
+++ zonas/bosque_de_elwynn/villadorada/espiritu_vaca.lua
@@ -0,0 +1,19 @@
+local EspirituVaca = {
+	entry = 90012
+};
+
+function EspirituVaca.OnGossipHello(event, player, object)
+    player:GossipClearMenu() -- required for player gossip
+    player:GossipMenuAddItem(0, '¿Estoy muuuuuuuuuuerta?', 1, 1)
+    player:GossipSendMenu(1, object, EspirituVaca.MenuId) -- MenuId required for player gossip
+end
+
+function EspirituVaca.OnGossipSelect(event, player, creature, sender, intid, code, menuid)
+    if (intid == 1) then
+        player:GossipComplete()     
+        creature:SendUnitSay('¡Muuuuuuuu!', 0)
+    end
+end
+
+RegisterCreatureGossipEvent(EspirituVaca.entry, 1, EspirituVaca.OnGossipHello)
+RegisterCreatureGossipEvent(EspirituVaca.entry, 2, EspirituVaca.OnGossipSelect)
