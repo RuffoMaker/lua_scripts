015874e8accede4980c936935215ec0ca06a7250
Pablo Lopez <esviasalpa@gmail.com>
2018-08-13 16:21:02 +0200

    Filcaun

--- zonas/mazmorras/purgatorio/filcaun.lua
+++ zonas/mazmorras/purgatorio/filcaun.lua
@@ -10,41 +10,22 @@ function filcaun.OnUpdate(event, creature, diff)
 
 filcaun.sumatorioDetiempo = filcaun.sumatorioDetiempo +diff
 
-if(filcaun.contadorInicio < filcaun.sumatorioDetiempo) then
-	if(creature:GetFriendlyUnitsInRange(3) ~= {}) then
-		creature:Emote(filcaun.emoteHablar)
-		creature:SendUnitSay(" ¡Pssss!,¡Pssss!,¡Pssss! ¡por aquí!", 0)
-		creature:SendUnitSay(" No hagaís ruido.Escape por los pelos... ese esbirro de Torcuato... ¡esta incluso más loco que él¡", 0)	
-		creature:SendUnitSay(" Escape graciás aún búfo que conseguir lanzarme que invente yo mismo.., es un poco impreciso aún y los resultados pueden variar..Pero si quereís os lo  lanzare a ustedes también, solo tiene que hablar conmigo todo el que quiera probar..,¡No seais tímidos!.", 0)
-		creature:SendUnitSay(" Como os dije aun esta en fase experimental... pero puede funcionar muy bien...", 0)
-		creature:Emote(filcaun.emoteNoHaceNada)
-		-- filcaun.contadorInicioDicho = filcaun.contadorInicioDicho + 1
-	end
-	filcaun.sumatorioDetiempo = 0
-else
+	if(filcaun.contadorInicio < filcaun.sumatorioDetiempo) then
+		if(creature:GetFriendlyUnitsInRange(3) ~= {}) then
+			creature:Emote(filcaun.emoteHablar)
+			creature:SendUnitSay(" ¡Pssss!,¡Pssss!,¡Pssss! ¡por aquí!", 0)
+			creature:SendUnitSay(" No hagaís ruido.Escape por los pelos... ese esbirro de Torcuato... ¡esta incluso más loco que él¡", 0)	
+			creature:SendUnitSay(" Escape graciás aún búfo que conseguir lanzarme que invente yo mismo.., es un poco impreciso aún y los resultados pueden variar..Pero si quereís os lo  lanzare a ustedes también, solo tiene que hablar conmigo todo el que quiera probar..,¡No seais tímidos!.", 0)
+			creature:SendUnitSay(" Como os dije aun esta en fase experimental... pero puede funcionar muy bien...", 0)
+			creature:Emote(filcaun.emoteNoHaceNada)
+			-- filcaun.contadorInicioDicho = filcaun.contadorInicioDicho + 1
+		end
+		filcaun.sumatorioDetiempo = 0
+	else
 	creature:SendUnitSay("En espera...", 0)
-end
-
-	
-  
+	end	 
 end
 
 
 
-function filcaun.OnUpdate2(event, creature, diff)
-
-filcaun.sumatorioDetiempo = filcaun.sumatorioDetiempo +diff
-
-if(filcaun.contadorInicio < filcaun.sumatorioDetiempo) then	
-	creature:SendUnitSay(filcaun.sumatorioDetiempo, 0)
-		
-		-- filcaun.contadorInicioDicho = filcaun.contadorInicioDicho + 1
-else
-	creature:SendUnitSay("En espera... ".. filcaun.sumatorioDetiempo, 0)
-end
-
-	
-  
-end
-
-RegisterCreatureEvent(80500, 7, filcaun.OnUpdate2)
+RegisterCreatureEvent(80500, 7, filcaun.OnUpdate)
