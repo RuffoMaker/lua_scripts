7c33b8b276fea3af025bb60b3d958a3ecf368df2
Pablo Lopez <esviasalpa@gmail.com>
2018-08-13 22:01:49 +0200

    filcaun

--- zonas/mazmorras/purgatorio/filcaun.lua
+++ zonas/mazmorras/purgatorio/filcaun.lua
@@ -8,13 +8,25 @@ local filcaun = {
 }
 
 function filcaun.OnUpdate(event, creature, diff)
-	filcaun.sumatorioDetiempo = filcaun.sumatorioDetiempo + diff
+
+filcaun.sumatorioDetiempo = filcaun.sumatorioDetiempo + diff
 
 	if(filcaun.tiempoHastaInicio < filcaun.sumatorioDetiempo) then
-		creature:SendUnitSay("hola",0)
+		if(creature:GetFriendlyUnitsInRange(3) ~= {}) then
+			creature:Emote(filcaun.emoteHablar)
+			creature:SendUnitSay(" ¡Pssss!,¡Pssss!,¡Pssss! ¡por aquí!", 0)
+			creature:SendUnitSay(" No hagaís ruido.Escape por los pelos... ese esbirro de Torcuato... ¡esta incluso más loco que él¡", 0)	
+			creature:SendUnitSay(" Escape graciás aún búfo que conseguir lanzarme que invente yo mismo.., es un poco impreciso aún y los resultados pueden variar..Pero si quereís os lo  lanzare a ustedes también, solo tiene que hablar conmigo todo el que quiera probar..,¡No seais tímidos!.", 0)
+			creature:SendUnitSay(" Como os dije aun esta en fase experimental... pero puede funcionar muy bien...", 0)		
+		-- filcaun.contadorInicioDicho = filcaun.contadorInicioDicho + 1
+		end
 		filcaun.sumatorioDetiempo = 0
+	else
+		creature:SendUnitSay("En espera...", 0)
 	end
+
 	
+  
 end
 
 
