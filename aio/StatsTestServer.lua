3573ea2b5572cc5e613fbf6fd8385604e9df8f5b
ReturnCero <rlopez@returncero.com>
2019-06-13 22:12:27 +0100

    AIO Stats

--- aio/StatsTestServer.lua
+++ aio/StatsTestServer.lua
@@ -28,8 +28,8 @@ AIO.AddOnInit(AddPlayerStats)
 
 local function UpdatePlayerStats(player)
 	local guid = player:GetGUIDLow()
-	local consulta = "UPDATE `puntos_alma` SET `restantes` = '"..AttributesPointsLeft[guid].."', `fuerza` = '"..AttributesPointsSpend[0].."', `agilidad` = '"..AttributesPointsSpend[1].."', `aguante` = '"..AttributesPointsSpend[2].."', `intelecto` = '"..AttributesPointsSpend[3].."', `espiritu` = '"..AttributesPointsSpend[4].."' WHERE `guid` = '"..guid.."';"
-	player:SendUnitSay(consulta, 0)
+	--local consulta = "UPDATE `puntos_alma` SET `restantes` = '"..AttributesPointsLeft[guid].."', `fuerza` = '"..AttributesPointsSpend[0].."', `agilidad` = '"..AttributesPointsSpend[1].."', `aguante` = '"..AttributesPointsSpend[2].."', `intelecto` = '"..AttributesPointsSpend[3].."', `espiritu` = '"..AttributesPointsSpend[4].."' WHERE `guid` = '"..guid.."';"
+	player:SendUnitSay(AttributesPointsSpend[0], 0)
 	CharDBQuery( "UPDATE `puntos_alma` SET `restantes` = '"..AttributesPointsLeft[guid].."', `fuerza` = '"..AttributesPointsSpend[0].."', `agilidad` = '"..AttributesPointsSpend[1].."', `aguante` = '"..AttributesPointsSpend[2].."', `intelecto` = '"..AttributesPointsSpend[3].."', `espiritu` = '"..AttributesPointsSpend[4].."' WHERE `guid` = '"..guid.."';" )
     AddPlayerStats(AIO.Msg(), player):Send(player)
 end
