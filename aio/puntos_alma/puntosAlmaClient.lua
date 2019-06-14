ce1c6d21989d51a4fc0227f8237d0fea42b4633b
ReturnCero <rlopez@returncero.com>
2019-06-14 08:00:40 +0100

    AIO Stats

--- aio/puntos_alma/puntosAlmaClient.lua
+++ aio/puntos_alma/puntosAlmaClient.lua
@@ -27,7 +27,7 @@ frameAttributes:Hide()
 
 -- Close button
 local buttonAttributesClose = CreateFrame("Button", "buttonAttributesClose", frameAttributes, "UIPanelCloseButton")
-buttonAttributesClose:SetPoint("TOPRIGHT", -5, -30)
+buttonAttributesClose:SetPoint("TOPRIGHT", -5, 30)
 buttonAttributesClose:EnableMouse(true)
 buttonAttributesClose:SetSize(27, 27)
 
